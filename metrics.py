ede395ddd81c23910e0f21c45ab9089f7c6bfb4b
Larry Zhu <larry.zhu@oracle.com>
2020-01-08 10:07:08 -0800

    update import

--- metrics.py
+++ metrics.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from elasticsearch import Elasticsearch, helpers
 from elasticsearch_dsl import Search, A
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import PythonOperator
+from airflow.operators.python_operator import PythonOperator
 from airflow import DAG
 
 url = "http://dev-iad-cluster-ingest.controltower:9200"
